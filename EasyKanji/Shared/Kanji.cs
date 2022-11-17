56460498aa48b3e7c91ff92c42298c5217c40270
fekoneko <55813967+fekoneko@users.noreply.github.com>
2022-11-17 09:47:55 +0300

    Added basic pages and navigation

---
+++ EasyKanji/Shared/Kanji.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyKanji.Shared
+{
+    public class Kanji
+    {
+        public char Writing { get; set; }
+        public string Meanings { get; set; }
+        public List<string> OnReadings { get; set; }
+        public List<string> KunReadings { get; set; }
+
+        public Kanji(char writing, string meanings, List<string> onReadings, List<string> kunReadings)
+        {
+            Writing = writing;
+            Meanings = meanings;
+            OnReadings = onReadings;
+            KunReadings = kunReadings;
+        }
+    }
+}
