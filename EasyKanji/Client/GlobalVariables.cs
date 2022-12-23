0c3d17fb20040a2e41915e6d23c9ae52d8a0d26d
fekoneko <55813967+fekoneko@users.noreply.github.com>
2022-12-23 22:37:41 +0300

    Repair some issues with bootstrap

---
+++ EasyKanji/Client/GlobalVariables.cs
@@ -0,0 +1,14 @@
+﻿using EasyKanji.Shared;
+
+namespace EasyKanji.Client
+{
+    static public class GlobalVariables
+    {
+        static public List<Kanji> LearnKanjiList { get; set; }
+
+        static GlobalVariables()
+        {
+            LearnKanjiList = new List<Kanji>();
+        }
+    }
+}
