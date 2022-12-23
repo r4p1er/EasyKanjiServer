eb610ffc694798413ead2211cfd83480db0a0c1f
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2022-12-24 00:16:21 +0300

    Changes in KanjisController
    Implemented method for converting Models.Kanji to Shared.Kanji; added search method for kanjis by writing, OnReadings, KunReadings, Meaning and words

--- EasyKanji/Server/Controllers/KanjisController.cs
+++ EasyKanji/Server/Controllers/KanjisController.cs
@@ -25,37 +25,59 @@ namespace EasyKanji.Server.Controllers
             var result = new List<Shared.Kanji>();
             foreach (var item in kanjis)
             {
-                char writing = item.Writing[0];
-
-                string meaning = item.Meaning;
-
-                var onItems = item.OnReadings.Split('0');
-                var onReadings = new List<string>();
-                foreach (var on in onItems)
-                {
-                    onReadings.Add(on);
-                }
-
-                var kunItems = item.KunReadings.Split('0');
-                var kunReadings = new List<string>();
-                foreach (var kun in kunItems)
-                {
-                    kunReadings.Add(kun);
-                }
-
-                var wordsPairs = item.Words.Split('0');
-                var words = new Dictionary<string, string>();
-                foreach (var pair in wordsPairs)
-                {
-                    string key = pair.Split('1')[0];
-                    string value = pair.Split('1')[1];
-                    words[key] = value;
-                }
-
-                result.Add(new Shared.Kanji(writing, meaning, onReadings, kunReadings, words) { Id = item.Id});
+                result.Add(ModelKanjiToSharedKanji(item));
             }
 
             return result;
+        }
+
+        [HttpGet]
+        [Route("search")]
+        public async Task<ActionResult<IEnumerable<Shared.Kanji>>> Search([FromQuery]string query)
+        {
+            var items = query.Split('、');
+            var kanjis = new List<Shared.Kanji>();
+            foreach (var item in items)
+            {
+                var selection = await db.Kanjis.Where(x => x.Writing == item || x.OnReadings.Contains(item) || x.KunReadings.Contains(item) || x.Meaning.Contains(item) || x.Words.Contains(item)).ToListAsync();
+                foreach (var k in selection)
+                {
+                    kanjis.Add(ModelKanjiToSharedKanji(k));
+                }
+            }
+            return kanjis.Distinct().ToList();
+        }
+
+        private Shared.Kanji ModelKanjiToSharedKanji(Models.Kanji kanji)
+        {
+            char writing = kanji.Writing[0];
+
+            string meaning = kanji.Meaning;
+
+            var onItems = kanji.OnReadings.Split('0');
+            var onReadings = new List<string>();
+            foreach (var on in onItems)
+            {
+                onReadings.Add(on);
+            }
+
+            var kunItems = kanji.KunReadings.Split('0');
+            var kunReadings = new List<string>();
+            foreach (var kun in kunItems)
+            {
+                kunReadings.Add(kun);
+            }
+
+            var wordsPairs = kanji.Words.Split('0');
+            var words = new Dictionary<string, string>();
+            foreach (var pair in wordsPairs)
+            {
+                string key = pair.Split('1')[0];
+                string value = pair.Split('1')[1];
+                words[key] = value;
+            }
+
+            return new Shared.Kanji(writing, meaning, onReadings, kunReadings, words) { Id = kanji.Id };
         }
     }
 }
