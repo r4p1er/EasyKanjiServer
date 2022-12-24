8fcc5a7216c97ae5b80548d805e891e57bf03c1f
fekoneko <55813967+fekoneko@users.noreply.github.com>
2022-12-24 07:07:55 +0300

    Learn Mode

---
+++ EasyKanji/Client/wwwroot/key-event.js
@@ -0,0 +1,14 @@
+﻿window.startKeyMonitoring = (objectReference) => {
+    document.onkeydown = (e) => {
+        e = e || window.event;
+        if (e.keyCode === 38) {
+            objectReference.invokeMethod('KeyHandler', 'up');
+        } else if (e.keyCode === 40) {
+            objectReference.invokeMethod('KeyHandler', 'down');
+        } else if (e.keyCode === 37) {
+            objectReference.invokeMethod('KeyHandler', 'left');
+        } else if (e.keyCode === 39) {
+            objectReference.invokeMethod('KeyHandler', 'right');
+        }
+    }
+}
